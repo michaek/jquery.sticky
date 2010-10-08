42704567c93bcb072df9802ccbc1f66e243c713a
Michael Hellein <michael@beardeds.com>
2010-10-08 10:50:49 -0400

    added placeholder to support layout flow below nav, added distance from top parameter

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -9,7 +9,8 @@
 (function($){
   $.fn.sticky = function(options){
     options = $.extend({
-      containedBy: null  // dom element that restricts scrolling of sticky
+      containedBy: null,  // dom element that restricts scrolling of sticky
+      distanceFromTop: 0
     }, options);
     
     // select the (optional) element the sticky is restricted by
@@ -20,8 +21,18 @@
       var $sticky = $(this);
       
       // get the original top and the original position
-      var stickyTop = $sticky.offset().top;
+      var stickyTop = $sticky.offset().top - options.distanceFromTop;
       var originalPosition = $sticky.css('position');
+
+      // still working on this - meant to support page flow remaining unchanged when float starts
+      // TODO: correctly adjust for margin - now just taking the top
+      var $stickyPlaceholder = $sticky.wrap('<div />').parent().css({
+        position: originalPosition,
+        width: $sticky.outerWidth(),
+        height: $sticky.outerHeight(),
+        marginTop: $sticky.css('margin-top')
+      });
+      $sticky.css('marginTop', '0');
 
       // on scroll, swap appropriate styles
       $(window).scroll(function(){
@@ -36,12 +47,12 @@
         // we're scrolling past the top of the sticky
         if (scrollOffset > stickyTop) {
           // if there's a bottom bound and we're scrolling past it
-          if (stickyBottom && scrollOffset > stickyBottom) {
+          if (stickyBottom && scrollOffset > stickyBottom - options.distanceFromTop) {
             // then position the sticky absolutely so it scrolls off the screen
             $sticky.css({position: 'absolute', top: stickyBottom});
           } else {
             // otherwise, keep that sticky in place!
-            $sticky.css({position: 'fixed', top: '0px'});
+            $sticky.css({position: 'fixed', top: options.distanceFromTop});
           }
         } else {
           // otherwise, we're above the stick's original position, so put everything back
